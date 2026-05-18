ddb2ee2ea3838d1874ce0a77335e2a6627890f37
Lumi <nicolasfreshromero15@gmail.com>
2026-05-18 14:12:26 -0300

    Creacion de modelos y realizar las migraciones

--- mi_proyecto/mi_app/models.py
+++ mi_proyecto/mi_app/models.py
@@ -1,3 +1,24 @@
 from django.db import models
 
-# Create your models here.
+class Banda(models.Model):
+    nombre = models.CharField(max_length=100)
+    genero_principal = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.nombre
+
+class Artista(models.Model):
+    nombre = models.CharField(max_length=100)
+    bandas_donde_participo = models.TextField()
+    genero_principal = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.nombre
+
+class Cancion(models.Model):
+    titulo = models.CharField(max_length=100)
+    fecha_lanzamiento = models.IntegerField() 
+
+    def __str__(self):
+        return self.titulo
+
