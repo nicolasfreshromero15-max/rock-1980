58ab60b535f5f679f2a29eb976a6eb2ae1186f04
Lumi <nicolasfreshromero15@gmail.com>
2026-05-18 14:19:02 -0300

    Implementacion del CRUD en views.py

--- mi_proyecto/mi_app/views.py
+++ mi_proyecto/mi_app/views.py
@@ -1,3 +1,26 @@
 from django.shortcuts import render
+from django.views.generic import ListView, CreateView, UpdateView, DeleteView
+from django.urls import reverse_lazy
+from .models import Artista
 
-# Create your views here.
+class ArtistaListView(ListView):
+    model = Artista
+    template_name = 'mi_app/artista_list.html'
+    context_object_name = 'artistas'
+
+class ArtistaCreateView(CreateView):
+    model = Artista
+    template_name = 'mi_app/artista_form.html'
+    fields = ['nombre', 'bandas_donde_participo', 'genero_principal']
+    success_url = reverse_lazy('artista_list')
+    
+    class ArtistaUpdateView(UpdateView):
+    model = Artista
+    template_name = 'mi_app/artista_form.html'
+    fields = ['nombre', 'bandas_donde_participo', 'genero_principal']
+    success_url = reverse_lazy('artista_list')
+
+class ArtistaDeleteView(DeleteView):
+    model = Artista
+    template_name = 'mi_app/artista_confirm_delete.html'
+    success_url = reverse_lazy('artista_list')
